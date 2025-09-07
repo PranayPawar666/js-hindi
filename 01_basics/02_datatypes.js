22cb20e1456b23c1ca2406ade18c1602d4a43762
Pranay Pawar <142098263+PranayPawar666@users.noreply.github.com>
2025-09-07 08:57:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3); //we are using node js, not browser
+
+console.log( 3 + 3); //code readability should be high
+
+console.log("Pranay");
+
+let name = "Pannu"
+let age = 23
+let isLoggedIn = false
+
+/*
+
+primitive datatypes
+
+// number => 2 to power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+*/
+
+
+// object =>
+
+console.log(typeof undefined); //output is undefined
+console.log(typeof null); // output is object
+
