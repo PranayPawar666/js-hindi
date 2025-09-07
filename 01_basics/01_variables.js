233828a90df43f74627b5513de5fbc739c862ee5
Pranay Pawar <142098263+PranayPawar666@users.noreply.github.com>
2025-09-07 07:49:03 +0000

    variable cofig done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 160616;
+let accountEmail = "pannu@gmail.com";
+var accountPassword = "12345";
+accountCity = "Chhatrapati sambhajinagar";
+let accountState;
+
+/// accountId = 01 //  Not allowed
+
+accountEmail ="pp@gmail.com"
+accountPassword = "010203"
+accountCity = "Sillod"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+bacause of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword,accountCity, accountState])
