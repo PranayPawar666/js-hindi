8e1a5be762f4da641d15d6b807047dfbc2e21259
Pranay Pawar <142098263+PranayPawar666@users.noreply.github.com>
2025-09-09 07:26:51 +0000

    Math and Num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+//================== Number ======================//
+
+// const score = 100;
+// console.log(score);
+
+// const balanced = new Number(30000);
+// console.log(balanced);
+
+// console.log(balanced.toString());
+// console.log(balanced.toString().length);
+
+// const newNumber = 123.2145;
+// console.log(newNumber.toPrecision(4));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++ //
+
+// console.log(Math);
+
+// console.log(Math.abs(-9));
+
+// console.log(Math.round(9.8));
+// console.log(Math.round(9.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.7));
+
+// console.log(Math.sqrt(64));
+
+// console.log(Math.min(3, 5, 9, 2));
+// console.log(Math.max(3, 5, 9, 2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()*(max - min +1)) + min );
