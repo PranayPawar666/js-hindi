4eb568390a76b094e9c51230b16c20c54b6a9f72
Pranay Pawar <142098263+PranayPawar666@users.noreply.github.com>
2025-09-10 06:37:37 +0000

    nums and math

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -31,11 +31,11 @@
 // console.log(Math.min(3, 5, 9, 2));
 // console.log(Math.max(3, 5, 9, 2));
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
-const min = 10;
-const max = 20;
+// const min = 10;
+// const max = 20;
 
-console.log(Math.floor(Math.random()*(max - min +1)) + min );
+// console.log(Math.floor(Math.random()*(max - min +1)) + min );
