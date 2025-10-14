d3f2d622d2a47dfc19a8ae3e3bba8e2037b72b50
Pranay Pawar <142098263+PranayPawar666@users.noreply.github.com>
2025-10-14 06:50:42 +0000

    nested function scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,56 @@ let a = 23
 if (true){
     let a = 10
     const b = 20
+    //console.log("INNER: ", a);
+    
 }
 
 
-console.log("My age is ",a);
-console.log(`My age is ${a}`);
+// console.log("My age is ",a);
+// console.log(`My age is ${a}`);
 //console.log(b);
+
+
+function one(){
+    const username = "Pranay"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+    
+}
+
+// one()
+
+if(true){
+    const username = "Pranay"
+    if(username === "Pranay"){
+        const website = " Youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ++++++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+console.log(addTwo(5));
+
+const addTwo = function(num){
+    return num + 2
+}
+
+
